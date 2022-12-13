513a5441229d2146ecc27ac886682bdfd4bcb4f6
Arif Burak Demiray <burakdemiray09@hotmail.com>
2022-12-13 14:41:33 +0300

    feat: todo and ccollection routers to main router

--- routes/endpoint.conf.js
+++ routes/endpoint.conf.js
@@ -1,11 +1,13 @@
-import usersRouter from './todo.controller.js'
 import authRouter from './auth.controller.js'
+import todoRouter from './todo.controller.js'
+import collectionRouter from './collection.controller.js'
 import cookieParser from 'cookie-parser';
 import express from 'express';
 
 export function subscribeEnpoints(app){
-    app.use('/',usersRouter)
     app.use('/',authRouter)
+    app.use('/',todoRouter)
+    app.use('/',collectionRouter)
 }
 
 export function configRequestTypes(app){
